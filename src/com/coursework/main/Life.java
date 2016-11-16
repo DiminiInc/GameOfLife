a77c914067929ac8b7ae4cbb7daa088716de33c1
kadavr95 <kadavr95@gmail.com>
2016-11-16 23:05:27 +0300

    -

--- src/com/coursework/main/Life.java
+++ src/com/coursework/main/Life.java
@@ -5,6 +5,8 @@ package com.coursework.main;
  */
 public class Life {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World 2!");
+        System.out.println("Hello, World 2!");
     }
+
 }

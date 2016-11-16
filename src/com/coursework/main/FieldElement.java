ddba124c5cb37bfca620dc994bbed233c7c7ba66
kadavr95 <kadavr95@gmail.com>
2016-11-17 00:02:24 +0300

    -

---
+++ src/com/coursework/main/FieldElement.java
@@ -0,0 +1,56 @@
+package com.coursework.main;
+
+/**
+ * Created by Yaskovich Dmitry on 16/11/2016.
+ */
+public class FieldElement {
+    private int channelRed, channelGreen, channelBlue;
+    private boolean alive;
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public int getChannelBlue() {
+        return channelBlue;
+    }
+
+    public int getChannelGreen() {
+        return channelGreen;
+    }
+
+    public int getChannelRed() {
+        return channelRed;
+    }
+
+    public void setAlive() {
+        this.alive = false;
+    }
+
+    public void setChannelBlue() {
+        this.channelBlue = 0;
+    }
+
+    public void setChannelGreen() {
+        this.channelGreen = 0;
+    }
+
+    public void setChannelRed() {
+        this.channelRed = 0;
+    }
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+
+    public void setChannelBlue(int channelBlue) {
+        this.channelBlue = channelBlue;
+    }
+
+    public void setChannelGreen(int channelGreen) {
+        this.channelGreen = channelGreen;
+    }
+
+    public void setChannelRed(int channelRed) {
+        this.channelRed = channelRed;
+    }
+}
--- src/com/coursework/main/Life.java
+++ src/com/coursework/main/Life.java
@@ -1,12 +1,85 @@
 package com.coursework.main;
-
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
 /**
  * Created by Yaskovich Dmitry on 16/11/2016.
  */
-public class Life {
-    public static void main(String[] args) {
-        System.out.println("Hello, World 2!");
-        System.out.println("Hello, World 2!");
+
+public class Life extends JFrame{
+
+    int countMilan;
+    int countReal;
+    JFrame frame =new JFrame("Game Of Life");
+    JButton btnMilan =new JButton("AC Milan");
+    JButton btnReal=new JButton("Real Madrid");
+    JLabel lblScore=new JLabel("Result: 0 x 0");
+    JLabel lblScorer=new JLabel("Last Scorer: N/A");
+    JLabel lblResult=new JLabel("Winner: DRAW");
+
+    public Life()
+    {
+        countMilan=0;
+        countReal=0;
+        InnerListener listener = new InnerListener();
+        btnMilan.addActionListener(listener);
+        btnReal.addActionListener(listener);
+
+        getContentPane().setBackground(new Color(200,200,200));
+        setSize(200,200);
+        setLayout(new FlowLayout());
+        add(btnMilan);
+        add(btnReal);
+        add(lblScore);
+        add(lblScorer);
+        add(lblResult);
+        setVisible(true);
+
+
+
+        FieldElement[][] Field = new FieldElement[100][100];
+
+
+
+
+    }
+    public static void main(String[] args)
+    {
+        new Life();
+    }
+
+    class InnerListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent ae)
+        {
+            if(ae.getSource() == btnMilan)
+            {
+                countMilan++;
+                lblScore.setText("Result: "+countMilan+" x "+countReal);
+                lblScorer.setText("Last Scorer: AC Milan");
+                if (countMilan>countReal)
+                    lblResult.setText("Winner: AC Milan");
+                else
+                if (countMilan<countReal)
+                    lblResult.setText("Winner: Real Madrid");
+                else
+                    lblResult.setText("Winner: Draw");
+            }
+            else
+            {
+                countReal++;
+                lblScore.setText("Result: "+countMilan+" x "+countReal);
+                lblScorer.setText("Last Scorer: Real Madrid");
+                if (countMilan>countReal)
+                    lblResult.setText("Winner: AC Milan");
+                else
+                if (countMilan<countReal)
+                    lblResult.setText("Winner: Real Madrid");
+                else
+                    lblResult.setText("Winner: Draw");
+
+            }
+        }
     }
 
 }

074db6f70a5a534360873c3059485d12de32c6cf
Dmitry Yaskovich <Dmitry Yaskovich>
2016-11-16 22:52:36 +0300

    Initial commit

---
+++ src/com/coursework/main/Life.java
@@ -0,0 +1,10 @@
+package com.coursework.main;
+
+/**
+ * Created by Yaskovich Dmitry on 16/11/2016.
+ */
+public class Life {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
